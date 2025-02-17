5c91a392692b32c0acc9e1bdf1bea49c0f579659
Christian Barkowsky <christian@plenta.io>
2025-02-17 14:19:56 +0100

    #1

---
+++ dca/tl_module.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_DCA']['tl_module']['palettes']['cars'] = '
+    {title_legend},name,headline,type;
+    {template_legend:hide},customTpl,imgSize;
+    {protected_legend:hide},protected;
+    {expert_legend:hide},cssID
+';
